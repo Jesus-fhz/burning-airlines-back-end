2fa3a1f1b1ee87a748cb50726c4014a0ccf68c8a
“Cassie <cassielee8757@gmail.com>
2022-02-15 21:05:09 +1100

    seeds ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,102 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Airplane.destroy_all
+
+a1 = Airplane.create!(
+    name: 'Airbus a380',
+    rows: 6,
+    columns: 20
+)
+
+a2 = Airplane.create!(
+    name: 'Boeing 747',
+    rows: 8,
+    columns: 18
+)
+
+a3 = Airplane.create!(
+    name: 'Airbus a320',
+    rows: 4,
+    columns: 10
+)
+
+
+
+Flight.destroy_all
+
+f1 = Flight.create!(
+    date_flight: "2022/03/10",
+    origin: 'Sydney',
+    destination: 'New York',
+    airplane_id: a1.id
+)
+
+f2 = Flight.create!(
+    date_flight: "2022/02/27",
+    origin: 'Sydney',
+    destination: 'Tokyo',
+    airplane_id: a2.id
+)
+
+f3 = Flight.create!(
+    date_flight: "2022/03/01",
+    origin: 'Sydney',
+    destination: 'London',
+    airplane_id: a3.id
+)
+
+User.destroy_all
+
+u1 = User.create!(
+    name: 'Cassie Lee',
+    email: 'cass@ga.co',
+    password: 'chicken',
+    admin: true
+)
+
+u2 = User.create!(
+    name: 'Jesus Flores',
+    email: 'jesus@ga.co',
+    password: 'chicken',
+    admin: true
+)
+
+u3 = User.create!(
+    name: 'Steven Furnari',
+    email: 'steven@ga.co',
+    password: 'chicken',
+    admin: true
+)
+
+u4 = User.create!(
+    name: 'Luke Hammer',
+    email: 'luke@ga.co',
+    password: 'chicken',
+    admin: false
+)
+
+Reservation.destroy_all
+
+Reservation.create!(
+    user_id: u1.id,
+    flight_id: f1.id,
+    row: 23,
+    column: 3
+)
+
+Reservation.create!(
+    user_id: u2.id,
+    flight_id: f2.id,
+    row: 20,
+    column: 5
+)
+
+Reservation.create!(
+    user_id: u3.id,
+    flight_id: f3.id,
+    row: 38,
+    column: 4
+)
+
+
+
+
+
