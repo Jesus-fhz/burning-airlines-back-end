aae20d7b9bd8f224c3594f7bdfd30da5b1f96e51
Steven Furnari <s.furnari@hotmail.com>
2022-02-15 19:24:33 +1100

    added row and column to reservation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_073025) do
+ActiveRecord::Schema.define(version: 2022_02_15_082314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2022_02_15_073025) do
     t.integer "flight_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "row"
+    t.integer "column"
   end
 
   create_table "users", force: :cascade do |t|
